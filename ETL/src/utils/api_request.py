d74629469742db7a22a2d51cdf95ec8cf3bc2d1f
Luciano Fodrini <luciano.fodrini@gmail.com>
2023-07-26 14:22:08 -0300

    .

--- ETL/src/utils/api_request.py
+++ ETL/src/utils/api_request.py
@@ -8,12 +8,14 @@ __req_session = requests.Session()
 def get_request(p_route, p_params=None):
     try:
         logger.info('Request HTTP GET: ' + p_route + ' - params: ' + str(p_params))
-        #response = requests.get(url=p_route, params=p_params)
         response = __req_session.get(url=p_route, params=p_params)
         
         if response.status_code != 200:
+            # Code 429 represent request limit exceeded
             if response.status_code == 429:
+                # Get the time for retry again
                 retry_after = response.headers.get("Retry-After") or API_RETRY_AFTER
+                # Wait the rate limit for retry request again
                 time.sleep(int(retry_after))
                 return get_request(p_route=p_route, p_params=p_params)
             else:
