74087d13f53997c0d0d7fb5011e6a243e2618552
Luciano Fodrini <luciano.fodrini@gmail.com>
2023-08-29 12:53:45 -0300

    email notificaction

--- App/dags/etl_nba/dag_etl_nba.py
+++ App/dags/etl_nba/dag_etl_nba.py
@@ -8,12 +8,6 @@ from etl_nba.src.etl import extract_data, transform_data, load_data
 from etl_nba.src.config import MAIL_TO
 from etl_nba.src.utils import send_mail
 
-default_args = {
-    'owner': 'lucianofodrini',
-    'retries': 5,
-    'retry_delay': timedelta(seconds=30)
-}
-
 # Email notification for success or failed dag/task
 def notify_email(context):
     ti = context['ti']
@@ -24,24 +18,50 @@ def notify_email(context):
 
     subject = ""
     body = ""
-
+    print('notify_email:' + dag_run._state)
+           
     if dag_run._state == State.SUCCESS: # Success email
-        subject = f"""Airflow: "{dag_run.dag_id}" succeed"""
+        subject = f"""Airflow OK: "{dag_run.dag_id}" succeed"""
         body = """
-            <p style='color:green'>DAG "{dag_id}" run successfully</p>
-            """.format(dag_id=dag_run.dag_id)
+            <h2>Mail message from Airflow</h2>
+            <hr />
+            <p>
+                DAG: {dag_id}<br>
+                Date: {dag_date}<br>
+                State: <span style='color:green'>Success</span><br>
+            </p>
+            """.format(dag_id=dag_run.dag_id, dag_date=dag_run.execution_date)
     else:
         if ti.state == State.FAILED: # Failed email
             logs_link = '{}/log?dag_id={}&task_id={}&execution_date={}'.format(conf.get("webserver", "base_url"), ti.dag_id, ti.task_id, quote(str(ti.execution_date)))
-            subject = f"""Airflow alert: "{dag_run.dag_id}" failed on "{ti.task_id}" """
+            subject = f"""Airflow ERROR: "{dag_run.dag_id}" failed on "{ti.task_id}" """
             body = """
-            <p style='color:red'>Task "{task_id}" failed.</p>
-            <p>Log: {log_link}</p>
-            """.format(task_id=ti.task_id, log_link=logs_link)
+                <h2>Mail message from Airflow</h2>
+                <hr />
+                <p>
+                    DAG: {dag_id}<br>
+                    Task: {task_id}<br>
+                    Date: {task_date}<br>
+                    State: <span style='color:red'>Failed</span><br>
+                    Log: {log_link}
+                </p>
+                """.format(dag_id=dag_run.dag_id, task_date=ti.execution_date, task_id=ti.task_id, log_link=logs_link)
 
     send_mail(p_to=recipient_emails, p_subject=subject, p_body=body)
 
 # Dag
+'''
+    owner: creator of de dag.
+    retries: number of retries.
+    retry_delay: time to try again.
+    on_failure_callback: function will be applied to all failed task.
+'''
+default_args = {
+    'owner': 'lucianofodrini',
+    'retries': 5,
+    'retry_delay': timedelta(seconds=30),
+    'on_failure_callback': notify_email
+}
 '''
     start_date: date will start being scheduled.
     schedule_interval: how the dag have to run. In this case, daily.
@@ -60,19 +80,19 @@ def notify_email(context):
 # Flow task of dag
 def task_flow_nba():
     # Task to extract data from api
-    @task(on_failure_callback=notify_email)
+    @task()
     def get_data():
         data = extract_data()
         return data
 
     # Task to transform data
-    @task(on_failure_callback=notify_email)
+    @task()
     def process_data(p_data):
         dfs = transform_data(p_data)
         return dfs
 
     # Task to load data to database
-    @task(on_failure_callback=notify_email)
+    @task()
     def save_data(p_dfs):
         load_data(p_dfs)
 
